3e457b57ad310998787af455577933a8d6321cc0
Corey DeRossett <cderossett3@gmail.com>
2024-02-01 11:21:42 -0500

    Added outputs for variables and output total cost of all seasons

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,13 @@ public class Main {
         int fallCost = 150;
         int winterCost = 200;
         int springCost = 50;
+        int totalCost = summerCost + fallCost + winterCost + springCost;
 
+        System.out.println("Your summer cost is " + summerCost);
+        System.out.println("Your fall cost is " + fallCost);
+        System.out.println("Your winter cost is " + winterCost);
+        System.out.println("Your spring cost is " + springCost);
+        System.out.println("Your total cost is " + totalCost);
 
-        System.out.println("Hello world!");
     }
 }
