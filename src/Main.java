44318a314f051ecf7b3cda9ffec3edd36416003c
Corey DeRossett <cderossett3@gmail.com>
2024-02-01 11:14:27 -0500

    Added variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
+        int summerCost = 100;
+        int fallCost = 150;
+        int winterCost = 200;
+        int springCost = 50;
+
+
         System.out.println("Hello world!");
     }
 }
