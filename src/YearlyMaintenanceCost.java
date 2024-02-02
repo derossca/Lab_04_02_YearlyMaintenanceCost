ffe55778630077d2d9dde01877313b5cad1ac7fb
Corey DeRossett <cderossett3@gmail.com>
2024-02-02 13:01:55 -0500

    renamed main java class and added comments

--- src/YearlyMaintenanceCost.java
+++ src/YearlyMaintenanceCost.java
@@ -1,12 +1,12 @@
-public class Main {
+public class YearlyMaintenanceCost {
     public static void main(String[] args) {
-
+        //Declared and initialized variables
         int summerCost = 100;
         int fallCost = 150;
         int winterCost = 200;
         int springCost = 50;
         int totalCost = summerCost + fallCost + winterCost + springCost;
-
+        //outputed all seasonal costs and total cost
         System.out.println("Your summer cost is " + summerCost);
         System.out.println("Your fall cost is " + fallCost);
         System.out.println("Your winter cost is " + winterCost);
